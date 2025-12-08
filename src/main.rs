80c551c7c0545331faaa27b110d3f5b34473beb2
Arshvir Goraya <113562877+ArshvirGoraya@users.noreply.github.com>
2025-12-08 16:21:06 -0500

    basic wasm logging setup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,75 @@
-use bevy::prelude::*;
+// use bevy::prelude::*;
+
+use bevy::log;
+use bevy::prelude::App;
+use bevy::prelude::Commands;
+use bevy::prelude::Component;
+use bevy::prelude::IntoScheduleConfigs;
+use bevy::prelude::Plugin;
+use bevy::prelude::Query;
+use bevy::prelude::Res;
+use bevy::prelude::ResMut;
+use bevy::prelude::Resource;
+use bevy::prelude::Startup;
+use bevy::prelude::Update;
+
+#[derive(Resource)]
+struct MyGreet(String);
+#[derive(Component)]
+struct MySayer(String);
 
 fn main() {
-    App::new().add_systems(Update, hello_world).run();
+    log::info!("hellope");
+    // println!("hello world 1");
+    // plugins can be used to add entire sections of your app at once.
+    App::new().add_plugins(MyWholeApp).run();
 }
 
-fn hello_world() {
-    println!("Hello, world!");
+pub struct MyWholeApp;
+
+impl Plugin for MyWholeApp {
+    fn build(&self, app: &mut App) {
+        app.add_plugins((
+            bevy::log::LogPlugin {
+                level: log::Level::DEBUG,
+                filter: "".to_string(),
+                // filter: "wgpu=error,bevy_render=info,bevy_ecs=trace".to_string(),
+                custom_layer: |_| None,
+                fmt_layer: |_| None,
+            },
+            bevy::app::PanicHandlerPlugin,
+        ));
+        app.insert_resource(MyGreet("Hello, World!".to_string())); // Resources must be inserted to add to global state.
+        app.add_systems(Startup, spawn_sayers); // components must be spawned to add to game world.
+        app.add_systems(
+            Update,
+            (sayer, change_my_sayer, change_my_greet, sayer, test_panic).chain(),
+        );
+        // Components and resources can be accessed/mutated by systems.
+        // .chain() can be used to enforce run order during. Otherwise all Update systems run in
+        // parallel.
+    }
+}
+
+fn spawn_sayers(mut commands: Commands) {
+    commands.spawn(MySayer("Rust".to_string()));
+}
+
+fn change_my_sayer(mut query: Query<&mut MySayer>) {
+    if let Some(mut sayer) = (&mut query).into_iter().next() {
+        sayer.0 = "Odin".to_string();
+    }
+}
+
+fn change_my_greet(mut my_greet: ResMut<MyGreet>) {
+    my_greet.0 = "Hellope!".to_string();
+}
+
+fn sayer(my_greet: Res<MyGreet>, query: Query<&MySayer>) {
+    log::info!("{} says {}", query.single().unwrap().0, my_greet.0)
+    // println!("{} says {}", query.single().unwrap().0, my_greet.0)
+}
+
+fn test_panic() {
+    panic!("THIS IS JUST A TEST DO NOT BE ALARMED!")
 }
