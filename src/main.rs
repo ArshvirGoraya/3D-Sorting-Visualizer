b3fe7cd526652883e1dd467700541adc3a627d95
Arshvir Goraya <113562877+ArshvirGoraya@users.noreply.github.com>
2025-12-20 23:19:28 -0500

    prevent default event handling when in canvas

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,7 @@ fn main() {
                 // present_mode: bevy::window::PresentMode::Fifo..Default::default(),
                 mode: bevy::window::WindowMode::Windowed,
                 fit_canvas_to_parent: true, // wasm "fullscreen"
-                prevent_default_event_handling: false,
+                prevent_default_event_handling: true,
                 ..Default::default()
             }),
             ..Default::default()
